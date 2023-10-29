5b535475d0b722e6d7d6f303e8bdb94edeefd2b7
A-Cute-Loli <146807353+A-Cute-Loli@users.noreply.github.com>
2023-10-29 22:26:32 +0800

    Updated Unity Project

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    private Rigidbody rb;
+    public GameObject player;
+    [SerializeField] private float velocity = 3.5f;
+    [SerializeField] private float rotationspeed = 10f;
+    [SerializeField] private GameManager GM;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
@@ -15,6 +22,43 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.velocity = Vector2.up * velocity;
+        }
+
+        if(player.transform.position.y >= 3.5f)
+        {
+            velocity = 0;
+        }
+        else if(player.transform.position.y <= -4.5f)
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+        else
+        {
+            velocity = 3.5f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, rb.velocity.y * rotationspeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Score")
+        {
+            GM.GetComponent<GameManager>().UpdateScore(1);
+        }
     }
 }
